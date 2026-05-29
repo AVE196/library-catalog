11471aa7350465619849b9fa901cdd10c43da737
Alexey <my@email.com>
2026-05-29 19:49:51 +0500

    implemented main class

---
+++ src/main/java/ru/ave/libraryCatalog/Printable.java
@@ -0,0 +1,6 @@
+package ru.ave.libraryCatalog;
+
+public interface Printable {
+
+    void printDetails();
+}
---
+++ src/main/java/ru/ave/libraryCatalog/Publication.java
@@ -0,0 +1,69 @@
+package ru.ave.libraryCatalog;
+
+import java.util.Objects;
+
+abstract public class Publication implements Printable {
+
+    private String title;
+    private String author;
+    int year;
+    private static int publicationCount;
+
+    public static int getPublicationCount() {
+        return publicationCount;
+    }
+
+    public abstract String getType();
+
+    public Publication(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        publicationCount++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Publication that = (Publication) o;
+        return year == that.year && Objects.equals(title, that.title) && Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
